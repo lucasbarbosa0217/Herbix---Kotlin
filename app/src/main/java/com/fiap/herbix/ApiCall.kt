e33f71529a81cf9e60280472a24afc4364bea0ad
lucasbarbosa0217 <lucasbarbosa0217@gmail.com>
2023-06-07 14:26:36 -0300

    Adicionar post de Token

--- app/src/main/java/com/fiap/herbix/ApiCall.kt
+++ app/src/main/java/com/fiap/herbix/ApiCall.kt
@@ -2,38 +2,88 @@ import android.util.Log
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import org.json.JSONObject
 import java.io.File
 import java.io.IOException
 import kotlin.math.log
 
 class ApiCall {
 
- fun uploadImage(imageFile: File) {
+    fun uploadImage(imageFile: File) {
         val client = OkHttpClient()
 
-        val requestBody = MultipartBody.Builder()
-            .setType(MultipartBody.FORM)
-            .addFormDataPart("image", imageFile.name, RequestBody.create("image/*".toMediaTypeOrNull(), imageFile))
+        getToken { token ->
+            if (token != null) {
+                val requestBody = MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart(
+                        "image",
+                        imageFile.name,
+                        RequestBody.create("image/*".toMediaTypeOrNull(), imageFile)
+                    )
+                    .build()
+                Log.w("Response API", token )
+
+
+                    val jsonObject = JSONObject(token)
+                     val tokenCortada = jsonObject.getString("access_token")
+
+
+
+                val request = Request.Builder()
+                    .url("https://www.nyckel.com/v1/functions/j4rq6vb5veq00lq6/invoke")
+                    .post(requestBody)
+                    .addHeader("Authorization", "Bearer "+tokenCortada)
+                    .build()
+
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        // Manipule a falha na solicitação aqui
+                        Log.e("API Response", "Request failed: ${e.message}")
+                    }
+
+                    override fun onResponse(call: Call, response: Response) {
+                        val responseBody = response.body?.string()
+                        // Manipule a resposta da solicitação aqui
+                        Log.d("API Response", "Response: $responseBody")
+                    }
+                })
+            } else {
+                // Lógica para lidar com falha ao obter o token
+                Log.e("API Response", "Failed to get token")
+            }
+        }
+
+    }
+    fun getToken(callback: (String?) -> Unit) {
+        val clientId = "a7jbecjr9hpfui0yui1wijijtfv42y1v"
+        val clientSecret = "lgfjb05ju7yvmbtvcxc67alqo0hzheoaftte2gntch2blmxhlzs5mvoauovbj62f"
+        val grantType = "client_credentials"
+
+        val formBody = FormBody.Builder()
+            .add("client_id", clientId)
+            .add("client_secret", clientSecret)
+            .add("grant_type", grantType)
             .build()
 
         val request = Request.Builder()
-            .url("https://www.nyckel.com/v1/functions/j4rq6vb5veq00lq6/invoke")
-            .post(requestBody)
-            .addHeader("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCJ9.eyJuYmYiOjE2ODU1NzkyODEsImV4cCI6MTY4NTU4Mjg4MSwiaXNzIjoiaHR0cHM6Ly93d3cubnlja2VsLmNvbSIsImNsaWVudF9pZCI6ImE3amJlY2pyOWhwZnVpMHl1aTF3aWppanRmdjQyeTF2IiwianRpIjoiQkQxQUIwREZGNjFGOTlCQkQwNjU2NTU4QUY1QTQ3MkQiLCJpYXQiOjE2ODU1NzkyODEsInNjb3BlIjpbImFwaSJdfQ.IQlZnFypE3Ury2zSgf8AvKmNfk73Y7e45XgE_W6nOQl5_qC9TUdqOaT6RkRqgMgry_L65bIn1okpauCMygCI-nCQqpEypNMOmT-4Z4fhM450INDprIFYKoiho1tyRxGbNcAQ-oH_VhbTqQHAthyrcLMcGnSYedKxvJO-CWE96wVcKH4qXUY1nqIa4e4t9hFl01Fa2vfAp12HmklcbZys-VrOJph0xIWYZoARPeRy2uApGdFpOXRjCei8hn2S7oyixRejD0stNceU0ufVxkA4-yQy67x79KAbo8a8jEAhXNE9Bc3uMxS6eQhG5LT4dsitmZGac1SIOVjQ3A4Su9Bkjw")
+            .url("https://www.nyckel.com/connect/token")
+            .post(formBody)
             .build()
 
+        val client = OkHttpClient()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                // Manipule a falha na solicitação aqui
-                Log.e("API Response", "Request failed: ${e.message}")
+                callback(null)
             }
 
             override fun onResponse(call: Call, response: Response) {
                 val responseBody = response.body?.string()
-                // Manipule a resposta da solicitação aqui
-                Log.d("API Response", "Response: $responseBody")
+                callback(responseBody)
             }
         })
     }
+
+
 
 }
